4d9cd52694ca49cdfb7e6689a2fbf057c5466107
Felipe Moreno <felipe@flpm.dev>
2023-09-16 12:47:14 -0400

    added stats

--- oak.py
+++ oak.py
@@ -1,20 +1,17 @@
 import typer
 
 import commands.add
+import commands.enrich
 from commands.stats import generate_stats
 
 
 app = typer.Typer()
 app.add_typer(commands.add.app, name="add", short_help="Add books to the library.")
-
-
-@app.command()
-def enrich(data_source: str):
-    """Enrich current books with data from a source."""
-    if data_source not in ["amazon"]:
-        typer.echo(f"Unsupported data source: {data_source}")
-        raise typer.Exit(code=1)
-    print("Enrich books using data from source:", data_source)
+app.add_typer(
+    commands.enrich.app,
+    name="enrich",
+    short_help="Enrich books already in the library.",
+)
 
 
 @app.command()
