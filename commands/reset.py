7fc309373cd07c895a4875c8ff402651d89b0b1a
Felipe Moreno <felipe@flpm.dev>
2023-10-22 18:29:22 -0400

    added reset command

---
+++ commands/reset.py
@@ -0,0 +1,9 @@
+from .utils.file import reset_catalogue
+
+
+def reset_library():
+    """
+    Reset the catalogue to an empty dictionary.
+    """
+    reset_catalogue()
+    print("Library reset.")
